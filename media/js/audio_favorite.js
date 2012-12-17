db3d97329c4a5e28abe589bf9ff77c873088c25f
Mohamed MADOUI <madoui@Macintosh.local>
2012-12-17 12:42:30 +0100

    Modifier le fichier audio_favorite.js
    Modifier le ficier audio_vaforite.js

--- media/js/audio_favorite.js
+++ media/js/audio_favorite.js
@@ -1,11 +1,29 @@
 jQuery(document).ready(function() {
+
+	function change_state (state) {
+		jQuery.post('index.php', {
+			option: 'com_hiplayer',
+			task: 'favorites.' + state,
+			format: 'json',
+			tour_id: jQuery('#audio').attr('rel'),
+			tmpl: 'raw'
+		}, function(response){
+			if (response.state == 'favorite') {
+				jQuery('#audio_favorite').removeClass('favorite_inactive')
+				.addClass('favorite_active').attr('rel', 'yes');
+			} else {
+				jQuery('#audio_favorite').removeClass('favorite_active')
+				.addClass('favorite_inactive').attr('rel', 'no');
+			};
+		});
+	}
+
 	jQuery('#audio_favorite').click(function() {
 		if (jQuery(this).attr('rel') == 'no') {
-			jQuery('#audio_favorite').removeClass('favorite_inactive')
-			.addClass('favorite_active').attr('rel', 'yes');
+			change_state('favorite');
 		} else {
-			jQuery('#audio_favorite').removeClass('favorite_active')
-			.addClass('favorite_inactive').attr('rel', 'no');
+			change_state('unfavorite');
 		};
 	});
 });
+
